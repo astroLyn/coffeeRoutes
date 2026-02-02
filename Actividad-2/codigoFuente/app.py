da403c316480bc4ea400211f1856cae466200d29
astrid <astrid4286@gmail.com>
2026-02-01 20:44:07 -0800

    Agrega Actividad 2

---
+++ Actividad-2/codigoFuente/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/mapa')
+def mapa():
+    # Renderiza el nuevo template del mapa
+    return render_template('map.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
